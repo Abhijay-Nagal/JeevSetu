9d6176db7789ecedc5da3a226cb076133ed75184
Abhijay-Nagal <nagalabhijay@gmail.com>
2026-08-30 18:35:14 +0530

    hide remaining illustration pages on mobile

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -284,7 +284,7 @@ export default function Home() {
           </Page>,
 
           /* Page 12 - Left Illustration (Action) */
-          <Page imagePage={true} imageSrc="/book/action.jpg" bookRef={bookRef} />,
+          !isMobile && <Page imagePage={true} imageSrc="/book/action.jpg" bookRef={bookRef} />,
 
           /* Page 13 - From Interest to Action */
           <Page number="6" bookRef={bookRef}>
@@ -319,7 +319,7 @@ export default function Home() {
           </Page>,
 
           /* Page 14 - Left Illustration (Final) */
-          <Page imagePage={true} imageSrc="/book/final.jpg" bookRef={bookRef} />,
+          !isMobile && <Page imagePage={true} imageSrc="/book/final.jpg" bookRef={bookRef} />,
 
           /* Page 15 - Your JeevSetu */
           <Page number="7" bookRef={bookRef}>
